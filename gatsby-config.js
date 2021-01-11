4506e6f84f6285abdbd5b65af4909bce0d323249
JMKJR <kuhn.johnmichael@gmail.com>
2021-01-11 14:42:18 -0500

    removed gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,14 +41,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-material-ui`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,20 @@
 import React from "react"
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
 import Footer from "../components/Footer"
 import { CssBaseline } from "@material-ui/core"
 import Box from "@material-ui/core/Box"
 import { Helmet } from "react-helmet"
 import Konami from "react-konami-code"
-import { useMediaQuery } from "react-responsive"
 import logo from "../images/bitcoinFAQLogo.png"
 import favicon from "../images/bitcoinFAQFavicon.png"
 import Categories from "../components/Categories"
 import CategorySelection from "../components/CategorySelection"
 import styles from "./index.module.css"
 
-export default function Home({ data }) {
+export default function Home() {
   function handleClick(contentTitle) {
     typeof window !== "undefined" &&
       window.gtag("event", "click", { clicked: contentTitle })
   }
-
-  const sources = [data.mobileImage.childImageSharp.fixed]
-
-  const isMobile = useMediaQuery({
-    query: "(max-width: 425px)",
-  })
 
   return (
     <div className="App">
@@ -55,11 +46,7 @@ export default function Home({ data }) {
       />
       <main>
         <Box p={7}>
-          {isMobile ? (
-            <Img fixed={sources} alt="BitcoinFAQ" />
-          ) : (
-            <img src={logo} alt="BitcoinFAQ" className={styles.logo} />
-          )}
+          <img src={logo} alt="BitcoinFAQ" className={styles.logo} />
         </Box>
         <CategorySelection onClick={handleClick} />
         <Categories onClick={handleClick} />
@@ -68,15 +55,3 @@ export default function Home({ data }) {
     </div>
   )
 }
-
-export const query = graphql`
-  query {
-    mobileImage: file(relativePath: { eq: "bitcoinFAQLogo.png" }) {
-      childImageSharp {
-        fixed(height: 70) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`
